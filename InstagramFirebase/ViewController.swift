a9ae53a08f052b8b903714e9cba6031ecc5b548b
Kayode Okusanya <okusanya.kayode@gmail.com>
2018-07-20 21:52:04 -0500

    Initial Commit

---
+++ InstagramFirebase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstagramFirebase
+//
+//  Created by OKUSANYA KAYODE DAMILARE on 7/20/18.
+//  Copyright © 2018 OKUSANYA KAYODE DAMILARE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
